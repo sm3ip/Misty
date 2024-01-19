4b95d4e45c320a12c1c3f9654664c7f568cb8d62
sm3ip <lucasomenpc@gmail.com>
2024-01-19 22:47:20 +0100

    basic project Setup

---
+++ chatBot/Source/chatBot/chatBotGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "chatBotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHATBOT_API AchatBotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
